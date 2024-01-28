c3d4b70b19c7de82e92f209d8e58c44eacf86acd
Pablo Escobar <PESCOB3@bancodebogota.com.co>
2024-01-28 11:10:13 -0500

    Sección 4 - App heros

---
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  public herosNames: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Thor', 'Captain America'];
+  public deletedHero?: string;
+
+  public removeLastHero(): void {
+    this.deletedHero = this.herosNames.pop();
+  }
+}
